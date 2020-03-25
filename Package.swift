f6a286709a59a1a9b815511f93f7a7d5a9eaf149
Alex Little <alex@onin.co>
2020-03-25 16:07:54 +0000

    Fix for mixed files

--- Package.swift
+++ Package.swift
@@ -18,11 +18,15 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+//        .target(
+//            name: "Tabman-Objc",
+//            path: ".",
+//            sources: ["Sources/Tabman"]),
         .target(
             name: "Tabman",
             dependencies: ["Pageboy"],
             path: ".",
-            sources: ["Sources/Tabman"]),
+            sources: ["Sources/Tabman/SwiftSources"]),
         .testTarget(
             name: "TabmanTests",
             dependencies: ["Tabman"]
